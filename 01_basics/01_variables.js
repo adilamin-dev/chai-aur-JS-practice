564ef29fab6ddb1bda1f37f877a8c1e38ee3512d
adilamin-dev <adilamin537@gmail.com>
2023-11-05 17:55:28 +0600

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 278518
+
+let accountEmail = "adilamin.dev@gmail.com"
+
+var accountPassword = "A@278518"
+
+accountCity = "Kishoreganj"
+
+let accountState;
+
+// accountId = 9 // not allowed
+
+accountEmail = "adil@hotmail.com"
+accountPassword = "00118844"
+accountCity = "Dhaka"
+
+/*
+Prefer not to use var
+brcause of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
