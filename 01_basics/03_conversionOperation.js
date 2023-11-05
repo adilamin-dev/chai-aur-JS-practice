acea8b2445e5f3a88f4cb4ffb1facbee8fbc2270
adilamin-dev <adilamin537@gmail.com>
2023-11-05 23:02:00 +0600

    Convertion and operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+// let score = false;
+
+// let valueInNumber = Number(score); // converting to number
+
+// console.table([typeof score, typeof(score)]);
+// console.table([typeof valueInNumber, typeof(valueInNumber), valueInNumber]);
+
+
+// "33 => 33"
+// "33abcd" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = 0;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn); // converting to boolean
+// console.log(booleanIsLoggedIn);
+
+
+// -------------- Operation ---------------
+
+let value = 3
+let newValue = -value
+// console.log(newValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Adil"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
+
+
+
+
+
+
